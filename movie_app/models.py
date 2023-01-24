910c37b01e0e15bca0ea1667896152faeba9beb6
Shaha00 <ahmadoshahzod438@gmail.com>
2023-01-24 19:49:31 +0600

    Accerted

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,20 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Director(models.Model):
     name = models.TextField(null=True, blank=True)
 
+    @property
+    def movie_count(self):
+        count = self.movie_count()
+        return count
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=180)
     description = models.TextField()
     duration = models.TextField()
-    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True, related_name='movie_count')
+
+    @property
+    def rating(self):
+        count = self.movie_reviews.count()
+        if count == 0:
+            return 0
+        total = sum(filter(None, [i.stars for i in self.movie_reviews.all()]))
+        return total / count
+
+
+CHOICES = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+)
 
 
 class Review(models.Model):
     text = models.TextField(null=True, blank=True)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True)
-
+    stars = models.IntegerField(choices=CHOICES)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name='movie_reviews')
--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -5,34 +5,36 @@ from .models import Director, Movie, Review
 class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
-        fields = 'id name'.split()
+        fields = 'id name movie_count'.split()
 
 
 class DirectorDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
-        fields = 'id name'.split()
-
-
-class MovieSerialize(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = 'id title'.split()
-
-
-class MovieDetailSerialize(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = 'id title description duration director'.split()
+        fields = 'id name movie_count'.split()
 
 
 class ReviewSerialize(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = 'id text'.split()
+        fields = 'id text stars'.split()
 
 
 class ReviewDetailSerialize(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = 'id text movie'.split()
+
+
+class MovieSerialize(serializers.ModelSerializer):
+    movie_reviews = ReviewSerialize(many=True)
+
+    class Meta:
+        model = Movie
+        fields = 'id movie_reviews rating title'.split()
+
+
+class MovieDetailSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = 'id title description duration director'.split()
